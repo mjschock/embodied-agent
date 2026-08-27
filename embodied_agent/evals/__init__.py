25ad65b7a82ca7d4ca8055a5e22ce884570e0009
Michael Schock <mjschock@users.noreply.github.com>
2026-08-26 20:07:30 -0700

    Add eval package exports

---
+++ embodied_agent/evals/__init__.py
@@ -0,0 +1,17 @@
+from .multi_robot import (
+    EvalCase,
+    EvalCaseResult,
+    EvalSuiteResult,
+    default_multi_robot_cases,
+    evaluate_cases,
+    run_scripted_baseline,
+)
+
+__all__ = [
+    "EvalCase",
+    "EvalCaseResult",
+    "EvalSuiteResult",
+    "default_multi_robot_cases",
+    "evaluate_cases",
+    "run_scripted_baseline",
+]
