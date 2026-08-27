47b76414b5fe4c3f70e195f1ad88b13c65efec8a
Michael Schock <mjschock@users.noreply.github.com>
2026-08-26 21:47:22 -0700

    Export shared world state models

---
+++ embodied_agent/world/__init__.py
@@ -0,0 +1,21 @@
+from .state import (
+    EntityFieldRef,
+    Pose3D,
+    TaskRunState,
+    TaskStatus,
+    TaskStepRecord,
+    WorldEntity,
+    WorldState,
+    entity_pose_refs,
+)
+
+__all__ = [
+    "EntityFieldRef",
+    "Pose3D",
+    "TaskRunState",
+    "TaskStatus",
+    "TaskStepRecord",
+    "WorldEntity",
+    "WorldState",
+    "entity_pose_refs",
+]
