e17deb7af729ac831b8ef128cb3563dfca80ce17
Michael Schock <mjschock@users.noreply.github.com>
2026-08-28 17:21:49 -0700

    Export SO-101 LeRobot adapter

--- embodied_agent/embodiments/__init__.py
+++ embodied_agent/embodiments/__init__.py
@@ -3,6 +3,7 @@ from .crazyflie_pybullet import CrazyfliePyBullet
 from .humanoid import HumanoidSim
 from .humanoid_mujoco import HumanoidMuJoCo
 from .microduck_mujoco import MicroduckMuJoCo
+from .so101_lerobot import SO101LeRobot, SO101ManipulationExecutor
 from .xlerobot import XLeRobotSim
 from .xlerobot_mujoco import XLeRobotMuJoCo
 
@@ -12,6 +13,8 @@ __all__ = [
     "HumanoidSim",
     "HumanoidMuJoCo",
     "MicroduckMuJoCo",
+    "SO101LeRobot",
+    "SO101ManipulationExecutor",
     "XLeRobotSim",
     "XLeRobotMuJoCo",
 ]
