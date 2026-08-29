da8062fb96ff55678c74fa4bf5b8978f7b3ff6a2
Michael Schock <mjschock@users.noreply.github.com>
2026-08-28 17:37:39 -0700

    Export Unitree G1 LeRobot adapter

--- embodied_agent/embodiments/__init__.py
+++ embodied_agent/embodiments/__init__.py
@@ -4,6 +4,7 @@ from .humanoid import HumanoidSim
 from .humanoid_mujoco import HumanoidMuJoCo
 from .microduck_mujoco import MicroduckMuJoCo
 from .so101_lerobot import SO101LeRobot, SO101ManipulationExecutor
+from .unitree_g1_lerobot import UnitreeG1LeRobot
 from .xlerobot import XLeRobotSim
 from .xlerobot_mujoco import XLeRobotMuJoCo
 
@@ -15,6 +16,7 @@ __all__ = [
     "MicroduckMuJoCo",
     "SO101LeRobot",
     "SO101ManipulationExecutor",
+    "UnitreeG1LeRobot",
     "XLeRobotSim",
     "XLeRobotMuJoCo",
 ]
